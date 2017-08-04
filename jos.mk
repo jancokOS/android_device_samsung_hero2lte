4142640a87be171e196da4418b1c71f4f810102c
bibahR <khabiburrokhman@gmail.com>
2017-08-04 10:46:01 +0000

    Initial jOS bringup

--- jos.mk
+++ jos.mk
@@ -14,7 +14,7 @@
 #
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
+$(call inherit-product, device/samsung/hero2lte/device.mk)
 $(call inherit-product, device/samsung/hero-common/hero-common.mk)
 
 # Inherit from the 64 bit configuration
@@ -24,10 +24,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/jos/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_hero2lte
+PRODUCT_NAME := jos_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_MODEL := SM-G935F
 PRODUCT_BRAND := samsung
